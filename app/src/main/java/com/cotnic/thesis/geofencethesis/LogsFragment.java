d1d607b27058be0b1c488b47ec53873c1b683fa0
Mitja Jeseničnik Kotnik <mitja@cotnic.com>
2017-08-20 14:18:16 +0200

    Log layout ready and functionalities. TODO: Print logs and save to external storage.

--- app/src/main/java/com/cotnic/thesis/geofencethesis/LogsFragment.java
+++ app/src/main/java/com/cotnic/thesis/geofencethesis/LogsFragment.java
@@ -7,16 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class LogsFragment extends Fragment {
+    private static final String TAG = LogsFragment.class.getSimpleName();
 
     private TextView mLogView;
     private View mView;
-    private Button mButtonGeofences;
-    private Button mButtonLocations;
-    private Button mButtonClearGeofence;
-    private Button mButtonClearLocation;
+    private Switch mSwitchGeofenceLog;
+    private Switch mSwitchLocationLog;
+    private Button mButtonShowLogs;
 
 
     @Override
@@ -31,6 +33,35 @@ public class LogsFragment extends Fragment {
 
         mLogView = (TextView) mView.findViewById(R.id.tvLogging);
         mLogView.setText("Start logging");
+
+        mSwitchGeofenceLog = (Switch) mView.findViewById(R.id.geofence_switch);
+        mSwitchGeofenceLog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.wtf(TAG, "Geofence is checked!");
+                } else {
+                    Log.wtf(TAG, "Geofence is unchecked!");
+                }
+            }
+        });
+
+        mSwitchLocationLog = (Switch) mView.findViewById(R.id.location_switch);
+        mSwitchLocationLog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.wtf(TAG, "Geofence is checked!");
+                } else {
+                    Log.wtf(TAG, "Geofence is unchecked!");
+                }
+            }
+        });
+
+        mButtonShowLogs = (Button) mView.findViewById(R.id.btnGeofence);
+        mButtonShowLogs.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.wtf(TAG, "Show data from log file about triggered Geofences");
+            }
+        });
 
         return mView;
     }
